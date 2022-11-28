5dd0e33f05b33616d2e2442de68f9d4a2bf7a201
darcy-murphy <113056025+darcy-murphy@users.noreply.github.com>
2022-11-27 20:34:55 -0500

    init

--- js/script.js
+++ js/script.js
@@ -1,3 +1,22 @@
-function myButtonClicked() {
-  document.getElementById("hello-world").innerHTML = "<p>Hello, World!</p>"
+// Copyright (c) 2022 Darcy Murphy All rights reserved
+//
+// Created by: Darcy Murphy
+// Created on: Nov 2022
+// This file contains the JS functions for index.html
+
+"use-strict"
+/**
+ * This function calculates the area and preimter of a trapezoid
+ */
+function calculated() {
+  // input
+  const height = parseInt(document.getElementById("sample3").value);
+  const baseb = parseInt(document.getElementById("sample2").value);
+  const basea = parseInt(document.getElementById("sample1").value);
+
+  // process 
+  const area = [(basea + baseb) / 2] * height;
+
+  // output
+  document.getElementById("area").innerHTML = "Area is:" + area + cm2;
 }
